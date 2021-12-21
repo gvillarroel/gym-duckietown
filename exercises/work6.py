58e8ef91576ff99a0703c13a5de34cb1ce8a6271
Gerardo Villarroel <villarroel.gj@gmail.com>
2021-12-21 18:48:38 -0300

    adding work6.py

--- exercises/work6.py
+++ exercises/work6.py
@@ -46,6 +46,8 @@ K = CAMERA_PARAMS
 CAMERA_WIDTH = 640
 CAMERA_HEIGHT = 480
 
+CENTER_WIDTH = CAMERA_WIDTH/2
+CENTER_HEIGHT = CAMERA_HEIGHT/2
 
 
 # 305.5718893575089, 0, 303.0797142544728,
@@ -153,9 +155,8 @@ def manual_control():
         action *= 1.5
 
     return action
-
-
-def seguidor_linea(blancas, amarillas):
+last_direction_blanca = 1
+def seguidor_linea(blancas, amarillas, patos):
     """
     """
     action = np.array([0.0, 0.0])
@@ -164,16 +165,35 @@ def seguidor_linea(blancas, amarillas):
     inserte aquí el código para que su duckiebot pueda seguir líneas
 
     """
-    velocity = 1
     y= 0
     b= 0.2
+    m = 4
+    y_blancas, y_amarillas, y_patos = (0,0,0,)
+    global last_direction_blanca
     if len(blancas) > 0:
-        #y = (1-blancas[0][2])*2
-        d = -1 if blancas[0][2] < 0 else 1
-        y = (blancas[0][2]*-8)+(b*d)
+        ro = blancas[:,1].mean()
+        theta = blancas[:,1].mean()
+        a = blancas[:,2].mean()
+        b = blancas[:,3].mean()
+        d = 1 if a <= 0 else -1
+        last_direction_blanca = d
+        avg = np.abs(a) 
+        y_blancas = np.abs(avg*m)*d
+    if len(patos) > 0 and patos[:,3].max() > 0.3*CAMERA_HEIGHT:
+        ARGMAX = patos[:,3].argmax()
+        #d = -1 if (patos[ARGMAX,0] + patos[ARGMAX,2]/2) < (CAMERA_WIDTH/2) else 1
+        d = last_direction_blanca
+        pos = d*(patos[ARGMAX,3]*m / CAMERA_HEIGHT)+(d*b)
+        y_patos = pos
+    else:
+        if len(amarillas) > 0:
+            avg = amarillas[:,2].mean()
+            d = 1 if avg < 0 else -1
+            avg = np.abs(1-avg)
+            y_amarillas = (avg*d)+(b*d)
+    y = y_blancas + y_amarillas + y_patos
+    print(f"({y_blancas:.2f}, {y_amarillas:.2f},{y_patos:.2f})")
     action = np.array([0.4, y])
-
-    print(blancas)
 
     return action
 
@@ -198,10 +218,19 @@ def detector_objetos(mask, image, min_area):
             detections.append((x, y, w, h, ))
     return np.array(detections)
 
-def detector_lineas(mask, image, min_area):
+def detector_lineas(mask, image, threshold):
     detections= []
+    mask[0:int(0.5*CAMERA_HEIGHT)] = 0
+    image[0:int(0.5*CAMERA_HEIGHT)] = 0
     edges = cv2.Canny(mask, 50, 150, apertureSize = 3)
-    lines = cv2.HoughLines(edges, 1, np.pi/180, 200)
+    """
+    'HoughLines(image, rho, theta, threshold[
+        , lines[, srn[, stn[, min_theta[, max_theta]]]]
+        ]) -> lines\n.   @brief Finds lines in a binary image using the standard Hough transform.\n.   \n.   The function implements the standard or standard multi-scale Hough transform algorithm for line\n.   detection. See <http://homepages.inf.ed.ac.uk/rbf/HIPR2/hough.htm> for a good explanation of Hough\n.   transform.\n.   \n.   @param image 8-bit, single-channel binary source image. The image may be modified by the function.\n.   @param lines Output vector of lines. Each line is represented by a 2 or 3 element vector\n.   \\f$(\\rho, \\theta)\\f$ or \\f$(\\rho, \\theta, \\textrm{votes})\\f$ . \\f$\\rho\\f$ is the distance from the coordinate origin \\f$(0,0)\\f$ (top-left corner of\n.   the image). \\f$\\theta\\f$ is the line rotation angle in radians (\n.   \\f$0 \\sim \\textrm{vertical line}, \\pi/2 \\sim \\textrm{horizontal line}\\f$ ).\n.   \\f$\\textrm{votes}\\f$ is the value of accumulator.\n.   @param rho Distance resolution of the accumulator in pixels.\n.   @param theta Angle resolution of the accumulator in radians.\n.   @param threshold Accumulator threshold parameter. Only those lines are returned that get enough\n.   votes ( \\f$>\\texttt{threshold}\\f$ ).\n.   @param srn For the multi-scale Hough transform, it is a divisor for the distance resolution rho .\n.   The coarse accumulator distance resolution is rho and the accurate accumulator resolution is\n.   rho/srn . If both srn=0 and stn=0 , the classical Hough transform is used. Otherwise, both these\n.   parameters should be positive.\n.   @param stn For the multi-scale Hough transform, it is a divisor for the distance resolution theta.\n.   @param min_theta For standard and multi-scale Hough transform, minimum angle to check for lines.\n.   Must fall between 0 and max_theta.\n.   @param max_theta For standard and multi-scale Hough transform, maximum angle to check for lines.\n.   Must fall between min_theta and CV_PI.'
+    """
+    lines = cv2.HoughLines(edges, 1, np.pi/180, threshold)
+    # lines = cv2.HoughLinesP(edges, 1, np.pi/180, threshold, 100,50)
+    
     #cv2.HoughLinesP() 
     if lines is None or len(lines) == 0:
         return np.array([])
@@ -215,44 +244,12 @@ def detector_lineas(mask, image, min_area):
         y1 = int(y0 + 1000*(a))
         x2 = int(x0 - 1000*(-b))
         y2 = int(y0 - 1000*(a))
-        cv2.line(image, (x1,y1), (x2,y2), (0, 0, 255), 1, cv2.LINE_AA)
-        #detections.append((x1, y1, x2, y2, [rho, theta, a, b]))
-        detections.append((rho, theta, a, b, ))
+        if np.abs(y1-y2) > 1:
+            cv2.line(image, (x1,y1), (x2,y2), (0, 0, 255), 1, cv2.LINE_AA)
+            detections.append((rho, theta, a, b, ))
 
     return np.array(detections)
 
-def detector_patos(image):
-    detections = []
-    #########################################################
-    # b) Bounding Boxes
-    MIN_AREA = 500
-    kernel = np.ones((5,5),np.uint8)
-    #Operacion morfologica erode
-    image_out = cv2.erode(image, kernel, iterations = 2)    
-    #Operacion morfologica dilate
-    image_out = cv2.dilate(image_out, kernel, iterations = 10)
-    global KEEP_GOIN
-    KEEP_GOIN = True
-    contours, hierarchy = cv2.findContours(image_out, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    for cnt in contours:
-            #Obtener rectangulo
-        x, y, w, h = cv2.boundingRect(cnt)
-        
-        #Filtrar por area minima
-        if w*h > MIN_AREA:
-            x2 = x + w  # obtener el otro extremo
-            y2 = y + h
-            #Dibujar un rectangulo en la imagen
-            cv2.rectangle(image2, (int(x), int(y)), (int(x2),int(y2)), (255,0,0), 3)
-            detections.append((x, y, w, h, ))
-        #########################################################
-        # c)  Freno de Emergencia
-        if h > CAMERA_HEIGHT * 0.33:
-            KEEP_GOIN = False
-    """
-    inserte aquí el código para que su duckiebot pueda detectar patos (filtro de color)
-    """
-    return detections
 
 
 run = True
@@ -291,20 +288,20 @@ while run:
     converted = cv2.cvtColor(obs, cv2.COLOR_RGB2HSV)
 
     # Filtrar colores de la imagen en el rango utilizando 
-    mask = cv2.inRange(converted, np.array([10,50,150]) , np.array([25,255,255]))
-    segment_image = cv2.bitwise_and(converted,converted, mask= mask)
-    image2 = cv2.cvtColor(segment_image, cv2.COLOR_HSV2BGR)
+    patos_mask = cv2.inRange(converted, np.array([10,50,150]) , np.array([25,255,255]))
+    segment_image = cv2.bitwise_and(converted,converted, mask=patos_mask)
+    patos = cv2.cvtColor(segment_image, cv2.COLOR_HSV2BGR)
 
-    #detector_patos(mask)
-    #cv2.imshow("patos", image2)
-    #cv2.waitKey(1)
+    cuadros_patos = detector_objetos(patos_mask, patos, 100)
+    cv2.imshow("patos", patos)
+    cv2.waitKey(1)
     
     #########################################################
     # Detectando la blanca
     blanca_mask = cv2.inRange(converted, np.array([0,0,150]) , np.array([180,25,255]))
     blanca_segment_image = cv2.bitwise_and(converted,converted, mask= blanca_mask)
     blanca = cv2.cvtColor(blanca_segment_image, cv2.COLOR_HSV2BGR)
-    lineas_blancas = detector_lineas(blanca_mask, blanca, 100)
+    lineas_blancas = detector_lineas(blanca_mask, blanca, 120)
     cv2.imshow("blancas", blanca)
     cv2.waitKey(1)
     #########################################################
@@ -312,7 +309,7 @@ while run:
     amarilla_mask = cv2.inRange(converted, np.array([25,80,10]) , np.array([50,255,255]))
     amarilla_segment_image = cv2.bitwise_and(converted,converted, mask= amarilla_mask)
     amarilla = cv2.cvtColor(amarilla_segment_image, cv2.COLOR_HSV2BGR)
-    lineas_amarillas = detector_lineas(amarilla_mask, amarilla, 100)
+    lineas_amarillas = detector_lineas(amarilla_mask, amarilla, 80)
     cv2.imshow("amarillas", amarilla)
     cv2.waitKey(1)
     #########################################################
@@ -321,7 +318,7 @@ while run:
         action = manual_control()
     
     if mode == 1:
-        action = seguidor_linea(lineas_blancas, lineas_amarillas)
+        action = seguidor_linea(lineas_blancas, lineas_amarillas, cuadros_patos)
 
     #########################################################
 
@@ -336,8 +333,8 @@ while run:
         env.reset()
 
 
-    #env.render(mode="top_down")
-    env.render()
+    env.render(mode="top_down")
+    #env.render()
     # env.render()
     time.sleep(1.0 / env.unwrapped.frame_rate)
 
